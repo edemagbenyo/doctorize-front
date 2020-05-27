0a51fbc681b811ba5dd2321f25a4d7fbae67b3c3
Edemone <edem.agbenyo@gmail.com>
2020-05-27 18:36:00 +0000

    add eslint

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+export default(<Router>
+  <Switch>
+    <Route path="/">
+      
+    </Route>
+  </Switch>
+</Router>)
