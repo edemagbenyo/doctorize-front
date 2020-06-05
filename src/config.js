edd6f6fdb8808662ce0a025a67d89b3774e749d2
Edemone <edem.agbenyo@gmail.com>
2020-06-05 02:23:12 +0000

    fix eslint error

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
-let url = "";
+let url = '';
 switch (process.env.NODE_ENV) {
-  case "development":
-    url = "http://localhost:3001";
+  case 'development':
+    url = 'http://localhost:3001';
     break;
-  case "production":
-    url = "https://doctorize-api.herokuapp.com/doctors";
+  case 'production':
+    url = 'https://doctorize-api.herokuapp.com/doctors';
     break;
   default:
-    url = "https://doctorize-api.herokuapp.com";
+    url = 'https://doctorize-api.herokuapp.com';
     break;
 }
 export { url };
