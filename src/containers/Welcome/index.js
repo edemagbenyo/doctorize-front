63fb410e7f85d4769ed310ba7c1af419a2bfbe28
Edemone <edem.agbenyo@gmail.com>
2020-06-11 16:21:50 +0000

    style button and welcome page

--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -9,22 +9,20 @@ import Doctor1 from '../../images/doctor1.png';
 const Welcome = ({ isLoggedIn }) => (
   <div className="welcome-container">
     <div className="navbar">
-      <div className="menu"></div>
+      <div className="menu"><h3>!Doctorize</h3></div>
       <div className="search"></div>
       <div className="account">
         <Account isLoggedIn={isLoggedIn} />
       </div>
     </div>
     <div className="slider">
+      <div className="slide"><img src={Doctor1} alt="Doctor" /></div>
       <div className="caption">
         <h1>Welcome to doctorize</h1>
-        <Link to="/">Get Started</Link>
+        <h2>Medical appointment at your convinience.</h2>
+        <h2>Talk to a medial specialist from the comfort of your home.</h2>
+        <Link to="/home">Get Started</Link>
       </div>
-      <div className="slide"><img src={Doctor1} alt="Doctor" /></div>
-    </div>
-
-    <div className="about">
-      <h2>KNow about us.</h2>
     </div>
 
   </div>
